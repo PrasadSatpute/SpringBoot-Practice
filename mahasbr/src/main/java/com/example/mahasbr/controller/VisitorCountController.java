20ccf6a987ed79d5e1268f58b9dd2d98ba0db94d
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2025-10-29 22:48:34 +0530

    Update

---
+++ mahasbr/src/main/java/com/example/mahasbr/controller/VisitorCountController.java
@@ -0,0 +1,32 @@
+package com.example.mahasbr.controller;
+
+import com.example.mahasbr.dto.VisitorCountDTO;
+import com.example.mahasbr.service.VisitorCountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/visitor")
+//@CrossOrigin(origins = "*")
+public class VisitorCountController {
+
+    private final VisitorCountService visitorCountService;
+
+    public VisitorCountController(VisitorCountService visitorCountService) {
+        this.visitorCountService = visitorCountService;
+    }
+
+    @PostMapping(value = "/increment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VisitorCountDTO> incrementVisitorCount() {
+        VisitorCountDTO result = visitorCountService.incrementVisitorCount();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/count", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VisitorCountDTO> getVisitorCount() {
+        VisitorCountDTO result = visitorCountService.getVisitorCount();
+        return ResponseEntity.ok(result);
+    }
+}
